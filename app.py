60c64158d8dc40e0a3d9f0030e4d59d3ff282533
Malik Bouaoudia <malik@bouaoudia.com>
2023-12-10 13:42:52 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    tasks.append(task)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
+
